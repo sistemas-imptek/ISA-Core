cc685363e9803c8951dfbb22ec62b8bcb0a22efc
sistemas-imptek <33266903+sistemas-imptek@users.noreply.github.com>
2018-04-13 15:20:54 -0500

    HCC MP Report OK

--- src/main/java/com/isacore/quality/report/GenerateReportPentahoHccMP.java
+++ src/main/java/com/isacore/quality/report/GenerateReportPentahoHccMP.java
@@ -56,8 +56,8 @@ public class GenerateReportPentahoHccMP extends AbstractReportGenerator {
 
 		final SQLReportDataFactory dataFactory = new SQLReportDataFactory(ReportConnection.ConnectionPentaho());
 		dataFactory.setQuery(QUERY_NAME, "select p.product_name, p.product_sap_code ,\r\n" + 
-				"hh.hcch_sapcode, hh.hcch_analysis, hh.hcch_code, hh.hcch_comment, hh.hcch_date, hh.hcch_norm, hh.hcch_batch, \r\n" + 
-				"hh.hcch_job, hh.hcch_periodicity, hh.hcch_reference, hh.hcch_review, hh.hcch_u_name, hh.hcch_work_area,\r\n" + 
+				"hh.hcch_sapcode, hh.hcch_analysis, hh.hcch_code, hh.hcch_comment, hh.hcch_date_order, hh.hcch_norm, hh.hcch_batch, \r\n" + 
+				"hh.hcch_job, hh.hcch_order_number, hh.hcch_reference, hh.hcch_review, hh.hcch_u_name, hh.hcch_work_area,\r\n" + 
 				"hd.hccd_prop_type, hd.hccd_norm_name, hd.hccd_prop_name, hd.hccd_prop_unit, hd.hccd_specifications, hd.hccd_test_result, hd.hccd_test_result_view, hd.hccd_pass_test\r\n" + 
 				"from dbo.product p\r\n" + 
 				"inner join dbo.hcchead hh on hh.product_id = p.product_id\r\n" + 
